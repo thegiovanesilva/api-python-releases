0f56d88832528082fc795c1f1d1abce2e0526b4b
Giovane Gonçalves Da Silva <giovane.silva@expressosaomiguel.com.br>
2022-09-14 14:20:09 -0300

    feat: add update version repository

--- app/repositories/update_version_repository.py
+++ app/repositories/update_version_repository.py
@@ -1,9 +1,24 @@
-from cmath import exp
-
-
+from bson import ObjectId
+from app.repositories.db.db_helper import Database
+database = Database.connect()["releases"]
 class UpdateVersionRepository:
-    def update_version(id, data):
+    def __init__(self, DB=database):
+        self.db = DB
+    def update(self, id, data):
         if not id:
             raise Exception('ID is required')
-        if not object:
+        if not data:
             raise Exception('Data to update is required')
+        
+        collection = self.db.get_collection('releases')
+        try:
+            collection.find_one_and_update({'_id': ObjectId(id)}, {'$set': data}) 
+            return {
+                'id': id,
+                'version': data['version'],
+                'date': data['date'],
+                'project': data['project']
+                }
+        except:
+            raise Exception('Version not found')
+        
