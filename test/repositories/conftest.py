e2aa13a1922f3e61be7b3b4f9e1c1a738353d76e
Giovane Gonçalves Da Silva <giovane.silva@expressosaomiguel.com.br>
2022-09-14 14:22:22 -0300

    add conftest to prevent stranger behavior

--- test/repositories/conftest.py
+++ test/repositories/conftest.py
@@ -1,10 +1,2 @@
-from importlib.resources import path
-from sys import path
-
 from app.repositories.db.db_helper import Database
 from app.repositories.load_version_repository import LoadVersionRepository
-
-
-
-
-# path.append(app)
