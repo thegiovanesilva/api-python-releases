730a6b894f8cc1ac95caaeff541fe3a8ff8d2ef1
Giovane Gonçalves Da Silva <giovane.silva@expressosaomiguel.com.br>
2022-09-14 14:17:37 -0300

    feat: ensure load will load versions in correct format

--- test/repositories/test_load_version_repository.py
+++ test/repositories/test_load_version_repository.py
@@ -8,25 +8,30 @@ client = MongoClient()
 db = client["releases"]
 collection = db["versions"]
 
+data = [
+    {"version": "1.0.0", "date": '2020-01-01', "project": "project1"},
+    {"version": "1.0.1", "date": '2020-01-02', "project": "project2"},
+    {"version": "1.0.2", "date": '2020-01-03', "project": "project3"},
+    {"version": "1.0.3", "date": '2020-01-04', "project": "project4"}
+]
+
 @pytest.fixture(autouse=True)
 def run_before_each_test():
     collection = db["versions"]
     yield collection
+    
 @pytest.fixture(autouse=True)
 def run_after_each_test():
     collection.drop()
+    yield
+
+@pytest.fixture(autouse=True)
+def run_after_all_tests():
     client.close()
     yield
 
 def test_load_all_versions():
     sut = LoadVersionRepository(db)
-    data = [
-    {"version": "1.0.0", "date": '2020-01-01', "project": "project1"},
-    {"version": "1.0.1", "date": '2020-01-02', "project": "project2"},
-    {"version": "1.0.2", "date": '2020-01-03', "project": "project3"},
-    {"version": "1.0.3", "date": '2020-01-04', "project": "project4"}
-    ]
-    
     mocked_data = []
     for item in data:
         object = {
@@ -34,10 +39,15 @@ def test_load_all_versions():
             'date': item['date'],
             'project': item['project']
         }
+        collection.insert_one(object)
         mocked_data.append(object)
-    collection.insert_many(data)
+    
     result = sut.load_all_versions()        
-    assert result == mocked_data
+    for index, item in enumerate(result):
+        assert item['id'] == str(mocked_data[index]['_id'])
+        assert item['version'] == mocked_data[index]['version']
+        assert item['date'] == mocked_data[index]['date']
+        assert item['project'] == mocked_data[index]['project']
 
 def test_load_all_versions_empty():
     sut = LoadVersionRepository(db)
@@ -47,36 +57,47 @@ def test_load_all_versions_empty():
 def test_load_version_by_date_inexistent():
     sut = LoadVersionRepository(db)
     result = sut.load_version_by_date('2020-01-01')        
-    assert result == None
+    assert result == []
 
 def test_load_version_with_valid_dates():
     sut = LoadVersionRepository(db)
-    data = [
-    {"version": "1.0.0", "date": '2020-01-01', "project": "project1"},
-    {"version": "1.0.1", "date": '2020-01-02', "project": "project2"},
-    {"version": "1.0.2", "date": '2020-01-03', "project": "project3"},
-    {"version": "1.0.3", "date": '2020-01-04', "project": "project4"}
-    ]    
-    collection.insert_many(data)
-    expected_data = [{"version": "1.0.0", "date": '2020-01-01', "project": "project1"}]
-    
-    result = sut.load_version_by_date('2020-01-01')        
-    assert result == expected_data
+    for item in data:
+        object = {
+            'version': item['version'],
+            'date': item['date'],
+            'project': item['project']
+        }
+        collection.insert_one(object)
+        
+    expected_data = collection.find({"date": '2020-01-01'})
+    result = sut.load_version_by_date('2020-01-01')
+            
+    for index, item in enumerate(result):
+        assert item['id'] == str(expected_data[index]['_id'])
+        assert item['version'] == expected_data[index]['version']
+        assert item['date'] == expected_data[index]['date']
+        assert item['project'] == expected_data[index]['project']
 
 def test_load_version_by_project_inexistent():
     sut = LoadVersionRepository(db)
     result = sut.load_version_by_project('inexistent_project')        
-    assert result == None
+    assert result == []
 
 def test_load_version_by_project_with_an_existent_project():
     sut = LoadVersionRepository(db)
-    data = [
-    {"version": "1.0.0", "date": '2020-01-01', "project": "project1"},
-    {"version": "1.0.1", "date": '2020-01-02', "project": "project2"},
-    {"version": "1.0.2", "date": '2020-01-03', "project": "project3"},
-    {"version": "1.0.3", "date": '2020-01-04', "project": "project4"}
-    ]    
-    collection.insert_many(data)
-    expected_data = [{"version": "1.0.0", "date": '2020-01-01', "project": "project1"}]
+    mocked_data = []
+    for item in data:
+        object = {
+            'version': item['version'],
+            'date': item['date'],
+            'project': item['project']
+        }
+        collection.insert_one(object)
+        mocked_data.append(object)
+    expected_data = collection.find({"date": '2020-01-01'})
     result = sut.load_version_by_project('project1')
-    assert result == expected_data    
+    for index, item in enumerate(result):
+        assert item['id'] == str(expected_data[index]['_id'])
+        assert item['version'] == expected_data[index]['version']
+        assert item['date'] == expected_data[index]['date']
+        assert item['project'] == expected_data[index]['project']
