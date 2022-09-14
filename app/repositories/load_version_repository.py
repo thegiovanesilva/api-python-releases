bf6a17b08d947f22fed36d0040705e90a0eb8a8d
Giovane Gonçalves Da Silva <giovane.silva@expressosaomiguel.com.br>
2022-09-14 14:16:07 -0300

    feat: add load version repository and its methods to load

--- app/repositories/load_version_repository.py
+++ app/repositories/load_version_repository.py
@@ -1,43 +1,22 @@
-from app.repositories.db.db_helper import Database 
+from app.repositories.db.db_helper import Database
+from app.utils.output_formatter import Formatter
 database = Database.connect()["releases"]
 
 class LoadVersionRepository:
     def __init__(self, DB=database): 
         self.db = DB
     def load_all_versions(self):
-        collection = self.db.get_collection('versions')
-        versions = collection.find()
-        result = []
-        for version in versions:
-            object = {
-                'version': version['version'],
-                'date': version['date'],
-                'project': version['project']
-            }
-            result.append(object)
-        return result
+        collection = self.db.get_collection('releases')
+        versions = collection.find()            
+        return Formatter.format(versions)
+
     def load_version_by_date(self, date):
-        collection = self.db.get_collection('versions')
+        collection = self.db.get_collection('releases')
         versions = collection.find({"date": date})
-        result = []
-        for version in versions:
-            object = {
-                'version': version['version'],
-                'date': version['date'],
-                'project': version['project']
-            }
-            result.append(object)
-            return result
+        return Formatter.format(versions)
+
     def load_version_by_project(self, project):
-        collection = self.db.get_collection('versions')
+        collection = self.db.get_collection('releases')
         versions = collection.find({"project": project})
-        result = []
-        for version in versions:
-            object = {
-                'version': version['version'],
-                'date': version['date'],
-                'project': version['project']
-            }
-            result.append(object)
-            return result
+        return Formatter.format(versions)
         
